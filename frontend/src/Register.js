2c677e8e9d3de659eb4856adc09613d6fcf88d6f
git-ronaldo <142833929+git-ronaldo@users.noreply.github.com>
2023-08-22 00:28:31 +0530

    Update Register.js

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -24,42 +24,21 @@ function Register() {
                 <form onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor="name"><strong>Name</strong></label>
-                        <input
-                            type="text"
-                            placeholder='Enter Name'
-                            name='name'
-                            onChange={(e) => setValues({ ...values, name: e.target.value })}
-                            className='form-control rounded-0'
-                        />
+                        <input type="text"  placeholder='Enter Name'  name='name' onChange={(e) => setValues({ ...values, name: e.target.value })} className='form-control rounded-0'/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
-                        <input
-                            type="email"
-                            placeholder='Enter Email'
-                            name='email'
-                            onChange={(e) => setValues({ ...values, email: e.target.value })}
-                            className='form-control rounded-0'
-                        />
+                        <input type="email"  placeholder='Enter Email'  name='email'  onChange={(e) => setValues({ ...values, email: e.target.value })}     
+                            className='form-control rounded-0' />
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
-                        <input
-                            type="password"
-                            placeholder='Enter password'
-                            name='password'
-                            onChange={(e) => setValues({ ...values, password: e.target.value })}
-                            className='form-control rounded-0'
-                        />
+                        <input  type="password" placeholder='Enter password'name='password' onChange={(e) => setValues({ ...values, password: e.target.value })}
+                            className='form-control rounded-0'/>
                     </div>
                     <button type='submit' className='btn btn-success w-100 rounded-0'>Sign up</button>
                     <p>You agree to our terms and policies</p>
-                    <Link
-                        to="/login"
-                        className='btn btn-default border w-100 rounded-0 text-decoration-none'
-                        style={{ backgroundColor: 'lightgrey', color: 'black' }}
-                    >
-                        Login
+                    <Link to="/login"className='btn btn-default border w-100 rounded-0 text-decoration-none'style={{ backgroundColor: 'lightgrey', color: 'black' }}>Login
                     </Link>
                 </form>
             </div>
